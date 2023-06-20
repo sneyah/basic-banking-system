f1531e8508fc8da25d516ad3682e8b68c3e182e7
sneyah <135428639+sneyah@users.noreply.github.com>
2023-06-20 13:37:57 +0530

    Update data.php

--- data.php
+++ data.php
@@ -2,7 +2,12 @@
 <html>
 <head>
 <title>Table with database</title>
-<link rel="stylesheet1" href="stylesheet1.css"/>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
 <style>
 html,
 body {
